643a4ff9d5dc9938c5ee25be04294c3dc71b6dcc
Agastya5Singh <2029142@kiit.ac.in>
2021-07-15 21:12:48 +0530

    day16

---
+++ react/day_16/day16.1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    return ( 
+        <div className = "card">
+        <div className = "image" >
+        <img src = "https://www.liveonegoodlife.com/wp-content/uploads/2020/03/word-image-61.jpeg"alt = "meme" >
+        </img> 
+        </div > 
+        <h1>Meme card</h1> 
+        <div className = "cardDescription"></div> 
+        </div >
+    );
+}
+
+export default App;
